328810799f5a3303467c9757b5ff4d8dbf88862f
Benoît Legat <benoit.legat@gmail.com>
2026-04-04 13:10:21 +0200

    Simplify

--- src/promote.jl
+++ src/promote.jl
@@ -1,11 +1,3 @@
-function _promote_variables_with_maps(vars_a, vars_b)
-    if vars_a == vars_b
-        return (vars_a, nothing), (vars_b, nothing)
-    end
-    all_vars = sort!(union(vars_a, vars_b))
-    return (all_vars, all_vars), (all_vars, all_vars)
-end
-
 function SA.promote_bases_with_maps(
     ::FullSpace,
     p::Union{MP.AbstractPolynomialLike,AbstractSemialgebraicSet},
@@ -17,28 +9,12 @@ function SA.promote_bases_with_maps(
     a::AbstractSemialgebraicSet,
     b::Union{MP.AbstractPolynomialLike,AbstractSemialgebraicSet},
 )
-    _a, _b = _promote_variables_with_maps(MP.variables(a), MP.variables(b))
+    _a, _b = MP.promote_variables_with_maps(MP.variables(a), MP.variables(b))
     return SA.maybe_promote(a, _a...), SA.maybe_promote(b, _b...)
 end
 
-function _promote_poly(p, new_vars)
-    return p * prod(v -> v^0, new_vars)
-end
-
-function _promote_polys(polys, new_vars)
-    return [_promote_poly(p, new_vars) for p in polys]
-end
-
-function SA.promote_with_map(
-    p::MP.AbstractPolynomialLike,
-    new_vars,
-    exponent_map,
-)
-    return _promote_poly(p, new_vars), exponent_map
-end
-
 function SA.promote_with_map(set::AlgebraicSet, new_vars, exponent_map)
-    new_polys = _promote_polys(equalities(set), new_vars)
+    new_polys = first.(SA.promote_with_map.(equalities(set), Ref(new_vars), Ref(exponent_map)))
     new_I = PolynomialIdeal(new_polys, set.I.algo)
     new_set = AlgebraicSet(new_I, set.solver)
     return new_set, exponent_map
@@ -54,6 +30,6 @@ end
 
 function SA.promote_with_map(set::BasicSemialgebraicSet, new_vars, exponent_map)
     new_V, _ = SA.promote_with_map(set.V, new_vars, exponent_map)
-    new_p = _promote_polys(inequalities(set), new_vars)
+    new_p = first.(SA.promote_with_map.(inequalities(set), Ref(new_vars), Ref(exponent_map)))
     return BasicSemialgebraicSet(new_V, new_p), exponent_map
 end
